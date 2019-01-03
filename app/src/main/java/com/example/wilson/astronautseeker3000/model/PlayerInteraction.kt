87935e741c7ca49db3f8bde34bb68d5d83322e3d
bcalone <bcalonesansouli@tf1.fr>
2019-01-03 14:22:16 +0100

    STEP BASE

--- app/src/main/java/com/example/wilson/astronautseeker3000/model/PlayerInteraction.kt
+++
@@ -1,8 +0,0 @@
-package com.example.wilson.astronautseeker3000.model
-
-sealed class PlayerInteraction {
-
-    object Rotate : PlayerInteraction()
-    class StopRotate(val winner: String) : PlayerInteraction()
-
-}
