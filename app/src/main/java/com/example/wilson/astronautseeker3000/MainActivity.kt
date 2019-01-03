4e4d69dea8c17b8b724c59faaf6b9528c65e469a
bcalone <bcalonesansouli@tf1.fr>
2019-01-03 14:32:24 +0100

    Step 2 : tout roule sans binding

--- app/src/main/java/com/example/wilson/astronautseeker3000/MainActivity.kt
+++ app/src/main/java/com/example/wilson/astronautseeker3000/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wilson.astronautseeker3000
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.adefruandta.spinningwheel.SpinningWheelView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,17 @@ class MainActivity : AppCompatActivity() {
 
         astronaut_spinning_wheel.items = listOf("Duck", "Racoon", "Panda", "Schizo")
         astronaut_spinning_wheel.isEnabled = true
+        astronaut_spinning_wheel.onRotationListener =
+                object : SpinningWheelView.OnRotationListener<String?> {
+                    override fun onRotation() {
+                    }
 
+                    override fun onStopRotation(item: String?) {
+                        item?.let { winner_text.text = getString(R.string.winner_text) + item }
+                    }
+
+                }
+
+        turn_the_wheel_button.setOnClickListener { astronaut_spinning_wheel.rotate(50f, 7000, 50) }
     }
 }
